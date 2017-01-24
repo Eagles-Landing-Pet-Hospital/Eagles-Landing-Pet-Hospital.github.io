ea4c485626c3edcdc0b8abf86bee82d17767a0ce
lebenswelt <bludragon12@gmail.com>
2017-01-24 03:01:18 -0500

    initial commit

---
+++ construction.php
@@ -0,0 +1,12 @@
+ <?php 
+ include 'php/head.php';
+ include 'php/menu_light.php';
+ include 'php/intro.php';
+ include 'php/contact.php';
+ include 'php/footer.php';?>
+
+<script src="http://eagleslandingpethospital.com/js/combo.js"></script>
+
+</body>
+
+
