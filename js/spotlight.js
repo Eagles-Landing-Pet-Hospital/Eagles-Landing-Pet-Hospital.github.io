2c8a462b360b9f40e4c8b77ac2484f5b98f22720
lebenswelt <bludragon12@gmail.com>
2017-02-03 12:03:38 -0500

    spotlight.js recover

---
+++ js/spotlight.js
@@ -0,0 +1,48 @@
+﻿function spotlight_connie() {
+document.getElementById('spotlight_img').innerHTML = 
+'<img src="resources/pics/spotlight_connie.png" width="400" height="250">';
+document.getElementById('spotlight').innerHTML = 
+'<h2>Connie Below Spencer, DVM</h2><br class="w1024 w640"><p style="font-size:20px;">Practice owner | Auburn University ’96 | In practice 20 years <br class="w1024 w640">Dr. Spencer graduated cum Laude from Auburn University’s College of Veterinary Medicine in 1996. She practiced as an associate to a well-known veterinarian and world-renowned breeder and AKC judge for several years, before relocating to Morehead in 2007. She is devoted to pet health, welfare, and vet medicine. Some of her current endeavors include:</p><br class="w1024 w640"><ul> <li class="job" style="font-size:20px;">Governor-appointed KVMA representative member of the Animal Control Advisory Board</li><li class="job" style="font-size:20px;">Adjunct Professor Emeritus with Morehead State University’s Vet Tech Program (surgery lab)</li><li class="job">Veterinary board member of the Rowan County Executive Shelter Board</li><li class="job">KVMA executive board member</li><li class="job">Buffalo Trace representative</li><li class="job">Wysong nutritional consultant</li></ul>'
+}
+
+function spotlight_todd() {
+document.getElementById('spotlight_img').innerHTML = 
+'<img src="resources/pics/spotlight_todd.png" width="400" height="250">'
+document.getElementById('spotlight').innerHTML = 
+'<h2>Todd Below</h2><br class="w1024 w640"><p style="font-size:20px;">Practice co-owner, office manager, military veteran | University of Kentucky<br class="w1024 w640">Todd was born in Henderson Kentucky but raised in California. He is Dr. Spencer\'s husband, the office manager, and practice co-owner. Before coming to ELPH to manage the team he served ten years in the military as a Marine and Army soldier. Todd can usually be found in his office taking care of the more subtle workings of running an animal clinic, but he can also be found helping out the staff with our more rowdy patients.</p>'
+}
+
+function spotlight_peyton() {
+document.getElementById('spotlight_img').innerHTML = 
+'<img src="resources/pics/spotlight_peyton.png" width="400" height="250">'
+document.getElementById('spotlight').innerHTML = 
+'<h2>Peyton Bailey</h2><br class="w1024 w640"><p style="font-size:20px;"Pre-vet student | Morehead State University ’19 | In practice 2 years | Pets: dogs Chico & Cooper<br class="w1024 w640">Peyton is a recent high school graduate now attending the MSU pre-vet program and is a member of the Pre-Vet Club. Her goal is to one day open her own practice that will be dedicated to the health of all types of animals. She would also like to take some time to work with large exotic cats.</p><br class="w1024 w640">'
+}
+
+function spotlight_britteny() {
+document.getElementById('spotlight_img').innerHTML = 
+'<img src="resources/pics/spotlight_britteny.png" width="400" height="250">'
+document.getElementById('spotlight').innerHTML = 
+'<h2>Britteny Hill, LVT</h2><br class="w1024 w640"><p style="font-size:20px;"BS vet tech | Morehead State University ’15 | In practice 5 years | Pets: cats Pickle & Ace<br class="w1024 w640">Britteny was born and raised in Louisville Kentucky. After completing her vet tech career program in high school, she went to MSU where she became a member of the Vet Tech Club, volunteered with ELPH, and obtained a BS in Veterinary Technology. Britteny is a licensed technician and now plans to return to MSU to pursue a Master\'s degree and become a vet tech instructor.</p><br class="w1024 w640">'
+}
+
+function spotlight_amanda() {
+document.getElementById('spotlight_img').innerHTML = 
+'<img src="resources/pics/spotlight_amanda.png" width="400" height="250">'
+document.getElementById('spotlight').innerHTML = 
+'<h2>Amanda Perkins</h2><br class="w1024 w640"><p style="font-size:20px;"Pre-vet student | Morehead State University ’16 | In practice: 3 years | Pet: fish Roger<br class="w1024 w640">Amanda originates from Ohio and is soon to graduate the MSU pre-vet program. She’ll be returning to her home state to attend Ohio State University’s school of veterinary medicine. We are sad to see her go but we’ll wish her all the best. Here are some of her other accomplishments:</p><br class="w1024 w640"><ul> <li class="job">Outstanding Pre-Vet Student ‘15</li> <li class="job">Mission trip to Costa Rica</li> <li class="job">Pre-Vet Club and Christian Vet Fellowship</li> <li class="job">Volunteering/working in clinics since she was 15</li></ul>'
+}
+
+function spotlight_kristina() {
+document.getElementById('spotlight_img').innerHTML = 
+'<img src="resources/pics/spotlight_kristina.png" width="400" height="250">'
+document.getElementById('spotlight').innerHTML = 
+'<h2>Kristina Pudlo, LVT</h2><br class="w1024 w640"><p style="font-size:20px;"BS vet tech | Morehead State University ’16 | In practice 6 years | Pets: cats Meta, Ollie, and Tybalt<br class="w1024 w640">Kristina is licensed technician and the newest addition to the ELPH staff. Originating from Germany and growing up primarily in Louisville, she has been surrounded by a large variety of pets all her life. She attended a high school with a four year vet tech career program while working for a Louisville veterinarian and has recently finished her BS at MSU. Now that Kristina has completed the program she intends to pursue a specialty in therapy and rehabilitation. Her interests are in exotics, pocket pets, and therapy.</p><br class="w1024 w640">'
+}
+
+function spotlight_vanessa() {
+document.getElementById('spotlight_img').innerHTML = 
+'<img src="resources/pics/spotlight_vanessa.png" width="400" height="250">'
+document.getElementById('spotlight').innerHTML = 
+'<h2>Vanessa Smith, LVT</h2><br class="w1024 w640"><p style="font-size:20px;"AAS vet tech | Morehead State University ’16 | In practice 5 years | Pets: dogs Shadow and Diesel<br class="w1024 w640">Vanessa is a wonderful member and licensed technician of the ELPH family. She originates from Indiana and like many of our other technicians has a love for animals. She is currently completing her BS of veterinary technology at MSU. Her major goal is to find herself working with large exotic cats or at a zoo.</p><br class="w1024 w640">'
+}
